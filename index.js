69f999341c3f43ad6094fc0b4de27c9adfd75354
Shihab Uddin <shihabuddin.dev@gmail.com>
2025-06-03 19:31:29 +0600

    initial setup server side

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// mongoDB
+const client = new MongoClient(process.env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("That's great! Server is running");
+});
+
+app.listen(port, (req, res) => {
+  console.log(`Server is running on port http://localhost:${port}`);
+});
